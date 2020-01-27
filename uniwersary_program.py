e879f0e4e2ddfe124ba8e67d7ef66e5c078a3699
KGorczyca <kmgorczyca@wp.pl>
2020-01-27 14:36:09 +0100

    change func 'who_celebrate_today'

--- uniwersary_program.py
+++ uniwersary_program.py
@@ -1,7 +1,16 @@
-import os, openpyxl, datetime, smtplib
+# -*- encoding: utf-8 -*-
+#! python3
+
+
+import os, json, openpyxl, datetime, smtplib, locale
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 today = datetime.date.today()
 today = today.strftime('%d.%m.%Y')  #konwersja obiektu datetime na ciąg tekstowy
+
+
 
 
 
@@ -13,11 +22,12 @@ def create_sheet(path, title_sheet, file_name_xlsx):    #utworzenie arkusza prog
     sheet.title = title_sheet
     
     list_with_date = []
-    for elem in range (1, int((len(file_with_date))+1)):
-        x = file_with_date[elem-1].split(' ')
+    for elem in range (1, (len(file_with_date)+1)):  #iteracja po pliku tekstowym z datami 
+        x = file_with_date[elem-1].split(' ')        #stworzenie zmiennej x zawierającej listę z imieniem, nazwiskiem oraz datą urodzin
         list_with_date.append(x)
+   
     numRow=1
-    for date in list_with_date:
+    for date in list_with_date:                      #iteracja przez zagnieżdżoną listę z danymi w celu stworzenia arkusza kalkulacyjnego
         for i in range (1, int((len(date))+1)):
             sheet.cell(row= numRow, column = i).value = date[i-1]
             sheet.column_dimensions['A'].width = 20
@@ -33,20 +43,27 @@ def create_sheet(path, title_sheet, file_name_xlsx):    #utworzenie arkusza prog
 
 
 
-def who_celebrate_today(title_sheet, file_name_xlsx):
+def who_celebrate_today(title_sheet, file_name_xlsx):  
 
     wb = openpyxl.load_workbook(file_name_xlsx)
     sheet = wb[title_sheet]
     i= sheet.max_row
-    
+    celebrate_list = []
     for j in range(1, i+1):
-        
-        birth = str(sheet.cell(row=j, column = 3).value)
-        if birth[0:5] == today[0:5]:
-            sentence = f'Today is  {int(today[6:10])-int(birth[6:10])} birthaday {sheet.cell(row=j, column = 1).value} {sheet.cell(row=j, column = 2).value}'.encode('utf-8')
-    return sentence
 
-def SendInfo(user,password, From, To, Subject, Body):
+    
+        birth = str(sheet.cell(row=j, column = 3).value)  #stworzenie zmiennej birth, zawierającej datę urodzenia
+        if birth[0:5] == today[0:5]:  #porównanie dnia i miesiąca urodzenia z dzisiejszą datą
+            sentence = f'Today is  {int(today[6:10])-int(birth[6:10])} birthaday {sheet.cell(row=j, column = 1).value} {sheet.cell(row=j, column = 2).value}'.encode('utf-8').decode('ascii','ignore')
+            celebrate_list.append(sentence)
+
+            
+    return celebrate_list
+    
+    
+    
+
+def SendInfo(user,password, From, To, Subject, Body):  #wysłanie wiadomości email z powiadomieniem o urodzinach
 
     message = f'From: {From} Subject: {Subject} \n{Body}'
     server = smtplib.SMTP_SSL('smtp.gmail.com')
@@ -62,10 +79,10 @@ def SendInfo(user,password, From, To, Subject, Body):
 
             
 print(create_sheet('date_file.txt','f.xlsx','FILE.xlsx'))
-   
 print(who_celebrate_today('f.xlsx','FILE.xlsx'))
-
-print(SendInfo('fikcyjnekontodozadan@gmail.com', '12345678KG','Kasia','fikcyjnekontodozadan@gmail.com', 'Birthday',who_celebrate_today('f.xlsx','FILE.xlsx') ))
+war = who_celebrate_today('f.xlsx','FILE.xlsx')  #przypisanie funkcji do zmiennej
+if war != False:                                 #stworzenie waruknu: jeżeli funkcja zwraca wartość różną od False wysłany zostaje mail z powiadomieniem o urodzinach
+    print(SendInfo('fikcyjnekontodozadan@gmail.com', '12345678KG','Kasia','fikcyjnekontodozadan@gmail.com', 'Birthday',who_celebrate_today('f.xlsx','FILE.xlsx') ))
 
 
 
